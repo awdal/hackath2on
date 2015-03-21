129948d16acd3b7467b7360ad7777d8cd2a7c141
xaviml <xaviml.93@gmail.com>
2015-03-21 22:48:14 +0100

    starting counter

--- HacktH2On/app/src/main/java/awdal/com/hackath2on/mainfragments/AutomatismFragment.java
+++ HacktH2On/app/src/main/java/awdal/com/hackath2on/mainfragments/AutomatismFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import awdal.com.hackath2on.R;
+
 public class AutomatismFragment extends Fragment {
 
     public static AutomatismFragment newInstance() {
--- HacktH2On/app/src/main/java/awdal/com/hackath2on/mainfragments/MainFragment.java
+++ HacktH2On/app/src/main/java/awdal/com/hackath2on/mainfragments/MainFragment.java
@@ -1,10 +1,15 @@
 package awdal.com.hackath2on.mainfragments;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import awdal.com.hackath2on.R;
+import awdal.com.hackath2on.otherfragments.CounterFragment;
 
 public class MainFragment extends Fragment {
 
@@ -25,6 +30,23 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final CounterFragment f = new CounterFragment();
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.add(R.id.card_view, f);
+        transaction.commit();
+
+
+        TextView cl = (TextView) v.findViewById(R.id.litres_minut);
+        cl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                f.startOrStopCounter();
+            }
+        });
+
+        return v;
     }
 }
