4eda3f731972765f888c5bb0a99883fc3a8ca7a3
Albert Folch <albertfolch58@hotmail.com>
2015-03-22 04:27:26 +0100

    client configurat per conectarse amb Folch

---
+++ BluetoothClient/app/src/main/java/com/example/albert/bluetoothclient/Constants.java
@@ -0,0 +1,13 @@
+package com.example.albert.bluetoothclient;
+
+/**
+ * Created by Albert on 21/03/2015.
+ */
+public class Constants {
+    public static final int ENABLED = 1;
+    public static final int DISABLED = 0;
+    public static final int NOT_SUPORTED = -1;
+    public static String MAC_MOBIL_SERVIDOR_XAVI = "48:59:29:08:5B:07";
+    public static String MAC_MOBIL_SERVIDOR_BRU = "C0:EE:FB:26:89:BA";
+    public static String MAC_MOBIL_SERVIDOR_FOLCH = "08:FC:88:54:EC:58";
+}
--- HacktH2On/app/src/main/java/awdal/com/hackath2on/MainActivity.java
+++ HacktH2On/app/src/main/java/awdal/com/hackath2on/MainActivity.java
@@ -99,14 +99,19 @@ public class MainActivity extends Activity implements MyInterface,Serializable{
     }
 
     public float getConsumActual() {
+
         switch (selected){
             case Constants.AIXETA1:
+                Log.e("connectat","aixeta1="+aixeta1);
                 return aixeta1;
             case Constants.AIXETA2:
+                Log.e("connectat","aixeta2="+aixeta2);
                 return aixeta2;
             case Constants.DUTXA:
+                Log.e("connectat","dutxa="+dutxa);
                 return dutxa;
             case Constants.GENERAL:
+                Log.e("connectat","general="+general);
                 return general;
         }
         return -1;
@@ -250,6 +255,7 @@ public class MainActivity extends Activity implements MyInterface,Serializable{
         Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
     }
     public void showMessageObtained(String text){
+        Log.e("connectat","he rebut:"+text);
         if(text.charAt(0) == '/'){//valor actual consum
             //2 aixetes,1 dutxa i 1general en aquest ordre, per exemple: /10.3,99.0,55.2,45.4
             String []split = text.substring(1).split(",");
@@ -257,7 +263,7 @@ public class MainActivity extends Activity implements MyInterface,Serializable{
             aixeta2 = Float.parseFloat(split[1]);
             dutxa  = Float.parseFloat(split[2]);
             general = Float.parseFloat(split[3]);
-            Log.e("connectat", text);
+
 
 
         }
